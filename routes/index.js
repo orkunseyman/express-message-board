10543c1ca7cced2bd6b1b2eb8df408bfbc253064
Orkun Seyman <orkunseyman@gmail.com>
2021-03-07 14:50:44 +0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title:'Mini Messageboard', messages: messages});
+});
+
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+router.post('/new', (req, res,next) => {
+  const messageText = req.body.title;
+  const messageUser = req.body.username;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = router;
